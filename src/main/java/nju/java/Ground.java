e994e452bdf92db8d5de91c0557ce3502e461896
151220129_WuZhengyi <18805156360@163.com>
2017-12-27 12:37:12 +0800

    init huluwa

---
+++ src/main/java/nju/java/Ground.java
@@ -0,0 +1,26 @@
+package nju.java;
+
+
+import javax.swing.JFrame;
+
+
+public final class Ground extends JFrame {
+
+    private final int OFFSET = 30;
+
+
+    public Ground() {
+        InitUI();
+    }
+
+    public void InitUI() {
+        Field field = new Field();
+        add(field);
+
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setSize(field.getBoardWidth() + OFFSET,
+                field.getBoardHeight() + 2 * OFFSET);
+        setLocationRelativeTo(null);
+        setTitle("Ground");
+    }
+}
